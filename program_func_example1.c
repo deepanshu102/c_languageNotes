02d45ca71cce6ac77ee9b4ff560a99d6e4df44cc
Deepanshu Gupta <deepanshujindal1021997@gmail.com>
2022-02-17 02:18:32 +0530

    function example 1

---
+++ program_func_example1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void hello(); // DEFINE PROTOTYPE OF FUNCTION
+int main(){
+    hello()   
+}
+void hello(){// FUNCTION BODY START
+    printf("Hello World") // FUNCTION DEFINATION 
+}//FUNCTION BODY END
